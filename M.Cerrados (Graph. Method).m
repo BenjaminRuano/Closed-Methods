b59e100439535f37c8d54074c6cbb71dee072a53
Benjamin <bruanoj02@gmail.com>
2025-03-20 16:23:08 -0600

    Add files via upload

---
+++ M.Cerrados (Graph. Method).m
@@ -0,0 +1,23 @@
+
+%comando clc limpia la memoria 
+clc; clear; close all;
+
+% Define the function
+f = @(x) x.^3 - 6*x.^2 + 11*x - 6;
+
+
+% Define the interval
+x = linspace( 0, 4, 1000);
+
+% Plot the function
+figure;
+plot(x, f(x));
+grid on;
+xlabel('x');
+ylabel('f(x)');
+title('Graphical Method: f(x) = x^3 - 6x^2 + 11x - 6');
+
+% Highlight the x-axis
+hold on;
+plot(x, zeros(size(x)), 'k--');
+hold off;
